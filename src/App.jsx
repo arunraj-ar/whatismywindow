c614c7557d4d769f3e0a3cc66970f4623cd38c7f
arunraj-ar <mail.arunrajar@gmail.com>
2024-10-31 23:49:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='font-mono h-dvh text-3xl lg:text-9xl flex items-center justify-center'>
+      Hello, World!
+    </div>
+    </>
+  )
+}
+
+export default App
